627962c4d1fbf5994f375918e66dca0bbbebd565
F5219817 <ifer.kam@fnb.co.za>
2023-07-11 15:47:39 +0200

    Add Details View

---
+++ puddle/core/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from item.models import Category, Item
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def index(request):
+    items = Item.objects.filter(is_sold=True)[0:6]
+    categories = Category.objects.all()
+
+    return render(request,'core/index.html', {
+        'categories': categories,
+        'items' : items,
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+
+def inde(request):
+  items = Item.objects.filter(is_sold=True)[0:6].values()
+  categories = Category.objects.all()
+  template = loader.get_template('core/index.html')
+  context = {
+    'items': items,   
+  }
+  return HttpResponse(template.render(context, request))      
